458580da46f3104ee91ee62b9cf110986205fa49
YxinMiracle <939157765@qq.com>
2024-07-21 19:36:07 +0800

    Site updated: 2024-07-21 19:36:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/16/Vue与React基础开发知识点总结，如组件编写方法、权限校验方式、监听变量改变等，极其适用于后端工程师。文章结尾有极度提高开发效率的彩蛋（二次封装基于antd的前端初始化模板）/","2024/07/16/hello-world/","2024/07/19/动态规划专栏/","2024/07/18/图算法-最短路径专栏/"];function toRandomPost(){
+var posts=["2024/07/21/MINT-Detecting-Fraudulent-Behaviors-from-Time-series-Relational-Data论文阅读笔记/","2024/07/16/Vue与React基础开发知识点总结，如组件编写方法、权限校验方式、监听变量改变等，极其适用于后端工程师。文章结尾有极度提高开发效率的彩蛋（二次封装基于antd的前端初始化模板）/","2024/07/16/hello-world/","2024/07/19/动态规划专栏/","2024/07/18/图算法-最短路径专栏/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
